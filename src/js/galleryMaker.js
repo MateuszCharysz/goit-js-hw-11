4f7a412030f53fa0bf73f367427623fad992dd9b
MateuszCharysz <charyszek@interia.pl>
2023-03-22 21:40:16 +0100

    imported modules to app

--- src/js/galleryMaker.js
+++ src/js/galleryMaker.js
@@ -1,5 +1,8 @@
 'use strict'
 import {log, httpCodeHandler, arrCompare, addLeadingZero} from './JsHelp'
+import simpleLightbox from 'simplelightbox'
+import Notiflix from 'notiflix'
+import axios from 'axios'
 (()=>{
 
 })()
